bade301fc7ef3188a1746d13664acb6f935d2099
Komal12 <upadhyay.ko@northeastern.edu>
2024-04-11 13:29:49 -0700

    Multiple user sessions

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
+import "dotenv/config";
 import express from "express";
+import session from "express-session";
 import mongoose from "mongoose";
 import UserRoutes from "./Kanbas/users/routes.js";
 import Hello from "./Hello.js";
@@ -7,9 +9,34 @@ import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from "./Kanbas/assignments/routes.js";
 import cors from "cors";
+
 mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  }),
+);
+
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false },
+};
+
+// if (process.env.NODE_ENV !== "development") {
+//   sessionOptions.proxy = true;
+//   sessionOptions.cookie = {
+//     sameSite: "none",
+//     secure: true,
+//     domain: process.env.HTTP_SERVER_DOMAIN,
+//   };
+// }
+app.use(session(sessionOptions));
+
 app.use(express.json());
 UserRoutes(app);
 ModuleRoutes(app);
